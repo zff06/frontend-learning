d8b5c53466e4e49bcdf62001e5dc7dd98c3e5705
zff06 <2395006165@qq.com>
2026-09-07 17:09:06 +0800

    feat: 02-vite-vue-demo vite 创建 vue3 项目

---
+++ 02-vite-vue-demo/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Vite 创建 Vue 项目</h2>
+  <p>{{ message }}</p>
+</template>
+​
+<script setup>
+let message = "学习 Vue 基础语法"
+</script>
+​
+<style>
+h2 {
+  color:#42b983;
+}
+</style>
